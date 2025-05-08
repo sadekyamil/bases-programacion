9c98890b918712eadca7d94ffa30db60ae624b2f
sadekyamil <sadekyamil@gmail.com>
2025-05-08 13:39:48 -0400

    kjk

--- ejemplo.py
+++ ejemplo.py
@@ -6,8 +6,6 @@ def mi_receta(ingrediente1, ingrediente2):
 primer_ingrediente = input()
 segundo_ingrediente = input()
 
-# Aquí estamos LLAMANDO a la función 'mi_receta'
-# y le estamos dando los 'ingredientes' (argumentos)
 combinacion = mi_receta(primer_ingrediente, segundo_ingrediente)
 
 print(f"La combinación es: {combinacion}")
